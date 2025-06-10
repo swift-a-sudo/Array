4ebe2b5210ca4799a9ccc079b2b50640089b972b
deepajadhav1107 <amoghajoshi2005@gmail.com>
2025-06-10 10:37:53 +0530

    save to the main

---
+++ ArrayOfScores.java
@@ -0,0 +1,10 @@
+public class ArrayOfScores {
+    public static void main (String[] args){
+    int a[]={11,21,34,46,68};
+    int sum= a[0]+a[1]+a[2]+a[3]+a[4];
+    int average=sum*5/100;
+    System.out.println("Sum of Array of scores: "+sum);
+        System.out.println("Average of Array of scores: "+average);
+
+    }
+}
